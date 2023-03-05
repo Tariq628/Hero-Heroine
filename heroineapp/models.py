cd9c119a8095b0e6dbbf101e15cca4b6f0b2ce22
tariq628 <tariqkaleri628@gmail.com>
2023-03-05 12:37:31 +0500

    Bug

--- heroineapp/models.py
+++ heroineapp/models.py
@@ -72,7 +72,7 @@ class Product(models.Model):
     buy_now = models.CharField(max_length=255, default='')
 
     def __str__(self):
-        return self.name
+        return self.name + " - " + self.category.name
 
 
 class ProductImg(models.Model):
